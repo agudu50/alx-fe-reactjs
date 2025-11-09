1274b750a9046fa273aa35354028062b6bb5bf55
agudu50 <anthonygudu2020@gmail.com>
2025-11-09 18:34:12 +0000

    updated my company project

---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav style={{
+      padding: "10px 20px",
+      backgroundColor: "#282c34",
+      display: "flex",
+      gap: "15px"
+    }}>
+      <Link to="/" style={{ color: "#fff", textDecoration: "none" }}>Home</Link>
+      <Link to="/about" style={{ color: "#fff", textDecoration: "none" }}>About</Link>
+      <Link to="/services" style={{ color: "#fff", textDecoration: "none" }}>Services</Link>
+      <Link to="/contact" style={{ color: "#fff", textDecoration: "none" }}>Contact</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
