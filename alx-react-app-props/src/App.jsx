9cd59178e566999dbe05315da9339c5f04e344f0
agudu50 <anthonygudu2020@gmail.com>
2025-11-09 17:29:47 +0000

    updated the UserProfile.js file

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
 import UserContext from './components/UserContext';
-
 import Footer from './components/Footer';
-
 import './App.css'
 
 import ProfilePage from './components/ProfilePage';
 
 function App() {
-  const [count, setCount] = useState(0);
+ 
   const [userData, setUserData] = useState({ name: "John Doe", email: "john@example.com" });
   
 
