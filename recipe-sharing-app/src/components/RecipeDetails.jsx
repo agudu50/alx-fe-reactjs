0acf457ac86d1182ee06474cd30b0ae1af4f7d23
agudu50 <anthonygudu2020@gmail.com>
2025-11-16 18:21:37 +0000

    updated the project

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -1,5 +1,5 @@
 ﻿import { useParams } from 'react-router-dom';
-import { useRecipeStore } from '../recipeStore';
+import { useRecipeStore } from './recipeStore';
 import EditRecipeForm from './EditRecipeForm';
 import DeleteRecipeButton from './DeleteRecipeButton';
 import FavoriteButton from './FavoriteButton';
