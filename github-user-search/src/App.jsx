06b023f2d5737244ccdf26db6c25b937119e2824
agudu50 <anthonygudu2020@gmail.com>
2025-11-22 17:19:29 +0000

    updated the githubServices file

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -35,7 +35,7 @@ function App() {
       {error && <p className="text-red-500 mt-4">Looks like we cant find the user</p>}
 
       {/* Results */}
-      <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-6 mt-6">
+      <div className="grid sm:grid-cols-2 md:grid-cols-4 gap-6 mt-6">
         {users.map((user) => (
           <div key={user.id} className="p-4 border rounded shadow bg-white">
             <img
