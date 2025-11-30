011eb7cd6497a4ecbd6ed4591786aef89ba57204
agudu50 <anthonygudu2020@gmail.com>
2025-11-30 17:03:19 +0000

    updated  user profile

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -9,7 +9,7 @@ function UserProfile() {
       mx-auto my-20 
       rounded-lg shadow-lg text-center">
 
-      <img src="https://via.placeholder.com/150" alt="User"   className="w-24 h-24 md:w-36 md:h-36 rounded-full mx-auto"/>
+      <img src="https://via.placeholder.com/150" alt="User"   className="sm:w-24 sm:h-24 md:w-36 md:h-36 rounded-full mx-auto"/>
       <h1 className="text-lg md:text-xl text-blue-800 font-semibold my-4">John Doe</h1>
       <p className= "text-sm md:text-base text-gray-600">Developer at Example Co. Loves to write code and explore new technologies.</p>
         </div>
