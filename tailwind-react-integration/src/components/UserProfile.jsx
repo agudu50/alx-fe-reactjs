263705300afdcd81e01038bd32d45ee7cddcf5d4
agudu50 <anthonygudu2020@gmail.com>
2025-11-30 17:37:27 +0000

    updated  user profile

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -3,7 +3,7 @@ function UserProfile() {
   return (
     
     <div className="user-profile">
-        <div className=" max-w-sm mx-auto bg-gray-100 p-8 my-20 rounded-xl shadow-md 
+        <div className=" max-w-xs md:max-w-sm mx-auto bg-gray-100 p-8 my-20 rounded-xl shadow-md 
       hover:shadow-lg transition-shadow duration-300 ease-in-out">  
         <div className="flex flex-col items-center">
       <img src="https://via.placeholder.com/150" alt="User"   className="sm:w-24 sm:h-24 md:p-8 sm:p-4 md:w-36 md:h-36 rounded-full object-cover mx-auto transition-transform duration-300 ease-in-out 
