468a7436d4ad3017bde215d46789c827750da2db
agudu50 <anthonygudu2020@gmail.com>
2025-11-30 16:27:54 +0000

    added a user profile

---
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -0,0 +1,16 @@
+
+function UserProfile() {
+  return (
+    
+    <div className="user-profile">
+        <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
+
+      <img src="https://via.placeholder.com/150" alt="User"  className="w-36 h-36 rounded-full mx-auto"/>
+      <h1 className="text-xl text-blue-800 font-semibold my-4">John Doe</h1>
+      <p className="text-gray-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
+        </div>
+    </div>
+  );
+}
+
+export default UserProfile;
